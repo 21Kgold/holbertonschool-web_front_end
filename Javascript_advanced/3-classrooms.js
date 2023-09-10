bc0d07406619cddbcc41b57606a95a71c7c73b7a
cau_rpa <claudiarpa@gmail.com>
2023-09-09 23:45:04 -0600

    Task 3 - Comment added

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -8,7 +8,7 @@ function createClassRoom (numbersOfStudents) {
 
     let students = [];
     for (let i = 0; i < numbersOfStudents; i++) {
-        students.push(studentSeat(i + 1));
+        students.push(studentSeat(i + 1));  // Generates an array of functions
     }
     return students;
 }
@@ -23,4 +23,7 @@ console.log(classRoom[9]());
 /*
 The studentSeat function may seem redundant, but without it, the script would
 return the error: Uncaught TypeError: classRoom[0] is not a function
+
+When the studentSeat function is not present, the createClassRoom function
+creates an array of integers and returns it.
 */
